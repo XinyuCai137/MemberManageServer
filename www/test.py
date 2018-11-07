d5593c39260f442a09ff3b91fcf77adcaebc3819
cxyvlf <cxyvlf@gmail.com>
2018-11-07 21:27:48 +0800

    Modify readme

---
+++ www/test.py
@@ -0,0 +1,12 @@
+import numpy as np
+p = 0.6
+q = 0.4
+
+
+# for i in range(1,26) :
+#     px = np.math.factorial(25) / (np.math.factorial(i) * np.math.factorial(25 - i))
+#     l = -np.log2(px * np.power(p, i) * np.power(1 - p, 25 - 1)) / 25
+#     print(i,px,l)
+# print((480700 + 1081575 + 2042975))# / np.power(2, 25))
+
+print(49*np.log2(3)/18 - 5 * np.log2(5)/15)
